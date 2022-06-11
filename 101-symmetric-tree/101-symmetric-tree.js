2cfc8eaf5e7e0a8f3a4df6e5d5c60e90f5662f02
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-06-11 19:36:19 +0300

    Time: 111 ms (24.95%), Space: 45 MB (18.49%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(!root) return true
+    const checkSymmetry = (left,right) =>{
+        if(!left && !right) return true
+        if(!left || !right) return false
+        if(left.val !== right.val) return false
+        return checkSymmetry(left.right,right.left) && checkSymmetry(left.left,right.right)
+    }
+    
+   return checkSymmetry(root.left,root.right)
+};
