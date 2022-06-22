f150defdc0878d2015a9798413b990cd292d2e68
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-06-22 15:23:11 +0300

    Time: 120 ms (5.17%), Space: 42.4 MB (24.45%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -7,12 +7,13 @@ var searchInsert = function(nums, target) {
     let start = 0
     let end = nums.length - 1
     if(target <= nums[0]) return 0
-    while(start < end){
+    while(true){
+        if(start>end) return start
         const mid = Math.floor((start+end)/2)
         if(target === nums[mid]) return mid
         else if(target < nums[mid]) end = mid -1
         else start = mid + 1
     }
-    return nums[start] >= target ? start : start+1
+    
     
 };
