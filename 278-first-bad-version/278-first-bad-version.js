6ba8f4bbc94602f39312ce6c24331dc7bbfda4d8
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-06-24 20:28:44 +0300

    Time: 82 ms (53.16%), Space: 41.8 MB (66.71%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -18,17 +18,16 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        let start = 1
-        let end = n
-        let mid = n
-        
-        while(start < end){
-            const mid = Math.floor((start+end) / 2)
-            
-            if(isBadVersion(mid) && !isBadVersion(mid-1)) return mid
-            else if( isBadVersion(mid) && isBadVersion(mid+1)) end = mid
-            else start = mid +1
-        }
-        return mid
+        // binary search
+       var start = 1, end = n;
+       while(start < end){
+           var mid = Math.floor(start + (end-start) / 2);
+           if(isBadVersion(mid)){
+                end = mid; // look on left side of mid
+           }else{
+               start = mid+1; // look on the right side of mid
+           }
+       }
+       return start;
     };
 };
