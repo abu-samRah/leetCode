d0f339124720273b539f4dca4355dff0c3dbcb30
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-06-26 13:30:43 +0300

    Time: 83 ms (48.28%), Space: 41.6 MB (85.39%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.js
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.js
@@ -12,16 +12,14 @@
  * @return {number}
  */
 var guessNumber = function(n) {
-    const binarySearch = (start,end) =>{
-        while(start<=end){
-            const mid = Math.floor((start+end)/2)
+        let high = n
+        let low = 1
+        while(low < high){
+            const mid = low +  Math.floor((high-low)/2)
             const guessedRes = guess(mid)
-            if(guessedRes === 0) return mid
-            else if(guessedRes === -1) end = mid -1
-            else start = mid + 1
+            if(guessedRes <= 0) high = mid
+            else low = mid + 1
         }
         
-        
-    }
-   return binarySearch(0,n)
+        return high
 };
