fd4a51e8ae9faa68e0bf88f426b1260807222146
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-06-26 13:36:50 +0300

    Time: 96 ms (31.31%), Space: 41.5 MB (92.21%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -12,6 +12,8 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
+
+
 var solution = function(isBadVersion) {
     /**
      * @param {integer} n Total versions
@@ -19,15 +21,12 @@ var solution = function(isBadVersion) {
      */
     return function(n) {
         // binary search
-       var start = 1, end = n;
-       while(start < end){
-           var mid = Math.floor(start + (end-start) / 2);
-           if(isBadVersion(mid)){
-                end = mid; // look on left side of mid
-           }else{
-               start = mid+1; // look on the right side of mid
-           }
+       var low = 1, high = n;
+       while(low < high){
+           const mid = low + Math.floor((high-low)/2);
+           if(isBadVersion(mid)) high = mid
+           else low = mid + 1
        }
-       return start;
+       return high;
     };
 };
