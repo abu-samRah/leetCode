c35313b4d8391598e7a374244f126be6c0127eec
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-06-26 14:11:08 +0300

    Time: 509 ms (17.35%), Space: 52.4 MB (78.57%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
@@ -7,13 +7,13 @@ var minimumSize = function(nums, maxOperations) {
   let i = 1
   let j = Math.max(...nums)
   
-  while (i <= j) {
-    let mid = Math.floor((j-i)/2 + i)
+  while (i < j) {
+    let mid = i + Math.floor((j-i)/2)
     let count = 0
     nums.forEach(n => count += Math.floor((n-1)/mid))
     
     if (count <= maxOperations) {
-      j = mid - 1
+      j = mid 
     } else {
       i = mid + 1
     }
