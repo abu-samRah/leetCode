fc737e072ab8e1b2bcf79b8df5a184b574e49300
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-06-26 13:59:52 +0300

    Time: 103 ms (18.70%), Space: 42.3 MB (30.85%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.js
+++ 162-find-peak-element/162-find-peak-element.js
@@ -3,16 +3,13 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    if (nums.length === 1 || nums[0] > nums[1]) return 0
-    if(nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1
+   let low = 0
+   let high = nums.length -1
+   while(low < high){
+       const mid = low + Math.floor((high - low) / 2)
+       if(nums[mid] > nums[mid+1]) high = mid
+       else low = mid + 1
+   }
     
-    const binarySearch = (start,end) =>{
-            if(start>end) return
-            const mid = Math.floor((start+end)/2)
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid
-            return binarySearch(start,mid-1) || binarySearch(mid+1,end)
-        }
-        
-    const res = binarySearch(0,nums.length-1)
-    return res
+    return high 
 };
