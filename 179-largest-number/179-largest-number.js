b4c2c677e95f0eb4fa8b2d437bb8e389c7b389e1
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-06-30 09:34:04 +0300

    Time: 73 ms (87.37%), Space: 44.2 MB (62.73%) - LeetHub

---
+++ 179-largest-number/179-largest-number.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    return nums.sort((a,b) => (b + '' + a ) - (a + '' + b)).join('').replace(/^0*/,'') || '0'
+    console.log(sortedArray )
+};
