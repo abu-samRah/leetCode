1539b610d06940160c672e21ceebaa23b097556f
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-10 13:14:29 +0300

    Time: 84 ms (82.95%), Space: 45.6 MB (66.36%) - LeetHub

---
+++ 690-employee-importance/690-employee-importance.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for Employee.
+ * function Employee(id, importance, subordinates) {
+ *     this.id = id;
+ *     this.importance = importance;
+ *     this.subordinates = subordinates;
+ * }
+ */
+
+/**
+ * @param {Employee[]} employees
+ * @param {number} id
+ * @return {number}
+ */
+var GetImportance = function(employees, id) {
+    const root = employees.find(employee => employee.id === id)
+    if(!root) return 0
+    const queue = []
+    queue.push(root)
+    const visited = {[id]:true}
+    let total = 0
+    while(queue.length){
+        const current = queue.shift()
+        total += current.importance
+        for(let i=0; i<current.subordinates.length; i++){
+            if(!visited[current.subordinates[i]]){
+                visited[current.subordinates[i]] = true
+                queue.push(employees.find(employee => employee.id === current.subordinates[i]))
+            }
+        }
+    }
+    
+    return total
+    
+};
