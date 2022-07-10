19c9099e43bbbf298039e48e0091d1fd9c3dbb53
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-10 10:55:54 +0300

    Time: 92 ms (57.14%), Space: 44.1 MB (76.87%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.js
+++ 841-keys-and-rooms/841-keys-and-rooms.js
@@ -2,21 +2,46 @@
  * @param {number[][]} rooms
  * @return {boolean}
  */
+
+class MyQueue extends Map {
+  constructor() {
+    super();
+    this.insertionIndex = 0;
+    this.removalIndex = 0;
+  }
+
+  enqueue(element) {
+    this.set(this.insertionIndex, element);
+    this.insertionIndex++;
+  }
+
+  dequeue() {
+    const el = this.get(this.removalIndex);
+    if (typeof el !== 'undefined') {
+      this.delete(this.removalIndex);
+      this.removalIndex++;
+    }
+    return el;
+  }
+    
+  size(){
+      return this.insertionIndex === this.removalIndex
+  }
+}
+
 var canVisitAllRooms = function(rooms) {
     const map = new Map()
-    const queue = []
-    queue.push(rooms[0])
+    const queue = new MyQueue()
+    queue.enqueue(rooms[0])
     map.set(0,true)
-    while(queue.length){
-        
-        const nodes = queue.shift()
+    while(!queue.size()){
+        const nodes = queue.dequeue()
         for(let i=0; i<nodes.length; i++){
             if(!map.get(nodes[i])){
                 map.set(nodes[i], true)
-                queue.push(rooms[nodes[i]])
+                queue.enqueue(rooms[nodes[i]])
             }
         }
     }
-
     return map.size === rooms.length 
 };
