73d6aa26a1f0226ac83a0ebfcd184a486005d264
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-10 13:41:02 +0300

    Time: 290 ms (40.81%), Space: 87.9 MB (77.24%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var deepestLeavesSum = function(root) {
+
+    const possible = []
+    const inOrder = (node,depth) =>{
+        if(node){
+            if(!node.left&&!node.right){
+                possible.push({depth,val:node.val})
+                return
+            }
+            inOrder(node.left, depth +1)
+            inOrder(node.right, depth +1)
+        }
+    }
+    inOrder(root,0)
+    const maxDepth = possible.reduce((acc,curr) => Math.max(acc,curr.depth),-1)
+    return possible.reduce((acc,curr) => {
+        if(curr.depth!==maxDepth){
+            return acc
+        }else{
+            return acc + curr.val
+        }
+    },0)
+    
+};
