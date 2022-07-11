0b167e122f00e971efc6073a06fd073ed0f37263
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-11 16:13:12 +0300

    Time: 412 ms (49.81%), Space: 49.1 MB (88.40%) - LeetHub

--- 127-word-ladder/127-word-ladder.js
+++ 127-word-ladder/127-word-ladder.js
@@ -9,29 +9,23 @@ var ladderLength = function(beginWord, endWord, wordList) {
     let queue = [beginWord];
     let steps = 1;
     
-    while(queue.length) {
-        const next = [];
-        
-        // loop over each word in the queue
-        for(let word of queue) {
-            if(word === endWord) return steps;
+   while (queue.length) {
+        const n = queue.length;
+        for (let i = 0; i < n; i++) {
+            const word = queue.shift();
+            if (word === endWord) return steps;
             
-            // loop over each char of the word 
-            for(let i = 0; i < word.length; i++) {
-                
-                // and replace the char with letters from [a - z]
-                for(let j = 0; j < 26; j++) {
-                    const newWord = word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i+1);
-                    
-                    // if the new word exist in the word list add it to the queue
-                    if(wordSet.has(newWord)) {
-                        next.push(newWord);
+            for (let j = 0; j < word.length; j++) {
+                for (let k = 0; k < 26; k++) {
+                    const newWord = word.slice(0, j) + String.fromCharCode(k + 97) + word.slice(j + 1);
+                    if (wordSet.has(newWord)) {
+                        queue.push(newWord);
                         wordSet.delete(newWord);
                     }
                 }
             }
         }
-        queue = next
+        
         steps++;
     }
     return 0;    
