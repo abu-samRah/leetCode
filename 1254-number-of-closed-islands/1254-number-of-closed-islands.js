526cf5ee1b14c8060f743982737f59a9639f88df
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-11 08:47:50 +0300

    Time: 115 ms (47.27%), Space: 44.2 MB (90.30%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.js
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.js
@@ -7,26 +7,24 @@ var closedIsland = function(grid) {
     let row = grid.length;
     let column = grid[0].length;
     for(let i = 0; i<row; i++){
-
         for(let j=0; j<column; j++){
-            if (grid[i][j] == 0){
-                if (helper(i,j))count++;
+            if (grid[i][j] === 0){
+                if (isClosedIsland(i,j))count++;
             }
         }
     }
 
-    function helper(i,j){
+    function isClosedIsland(i,j){
         if (i < 0 || j < 0 || i>=row || j>=column) return false;
         if (grid[i][j]) return true;
         grid[i][j] = true;
 
-        let top = helper(i - 1, j)
-        let bottom = helper(i + 1, j)
-        let left = helper(i, j-1)
-        let right = helper(i, j + 1);
+        let top = isClosedIsland(i - 1, j)
+        let bottom = isClosedIsland(i + 1, j)
+        let left = isClosedIsland(i, j-1)
+        let right = isClosedIsland(i, j + 1);
         return top && bottom && left && right;
 
     }
-    console.log(grid)
     return count
 };
