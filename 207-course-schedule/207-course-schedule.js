8ab840b3b7ea142c33088749fdd9ac7f24d12cca
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-14 09:09:11 +0300

    Time: 130 ms (45.54%), Space: 45.6 MB (92.40%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+
+const VISITED = 2, VISITING = 1, UNVISITED = 0;
+
+var canFinish = function(numCourses, prerequisites) {
+    const connections = prepareInput(numCourses,prerequisites);
+    return solution(connections)
+    
+    
+};
+
+const prepareInput = (numCourses,prerequisites) =>{
+    const connections = new Array(numCourses).fill([])
+    for(let i=0; i<prerequisites.length; i++){
+        const curr = connections[prerequisites[i][1]]
+        if(connections[prerequisites[i][1]].length) 
+            connections[prerequisites[i][1]].push(prerequisites[i][0]) 
+        else{
+            connections[prerequisites[i][1]] = []
+            connections[prerequisites[i][1]].push(prerequisites[i][0]) 
+        }
+    }
+    return connections
+}
+
+function solution(connections) {
+    const visitedList = new Array(connections.length).fill(UNVISITED)
+    
+    for(let index=0; index<connections.length; index++){
+        if(visitedList[index] === UNVISITED) 
+            if(!dfs(index,connections,visitedList)) 
+                return false
+    }
+
+    return true
+}
+
+const dfs = (index,connections,visitedList) =>{
+    visitedList[index] = VISITING
+    for(let neighbor of connections[index]){
+        if(visitedList[neighbor] === VISITING) return false
+        if(visitedList[neighbor] === UNVISITED)
+            if(!dfs(neighbor,connections,visitedList)) 
+                return false
+    }
+    visitedList[index] = VISITED
+    return true
+}
