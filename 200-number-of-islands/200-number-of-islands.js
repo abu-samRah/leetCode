8e6ed52ddb5fcc50a1a5852b476f5ec8035f04db
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-14 09:59:06 +0300

    Time: 109 ms (72.19%), Space: 45 MB (64.96%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -21,12 +21,9 @@ const dy = [0,0,1,-1]
 
 const dfs = (grid, i, j) =>{
     if( i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] === "0" ) return 0
-    
-        grid[i][j] = "0"
-        dfs(grid,i+1,j)
-        dfs(grid,i-1,j)
-        dfs(grid,i,j+1)
-        dfs(grid,i,j-1)
-    
-    
+    grid[i][j] = "0"
+    dfs(grid,i+1,j)
+    dfs(grid,i-1,j)
+    dfs(grid,i,j+1)
+    dfs(grid,i,j-1)
 }
