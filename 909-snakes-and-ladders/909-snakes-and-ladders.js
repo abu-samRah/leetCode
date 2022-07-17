36f67bd7fa1d43ae857d85e7bc7623d41e9a7881
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-17 18:08:00 +0300

    Time: 131 ms (38.14%), Space: 48.4 MB (41.24%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.js
+++ 909-snakes-and-ladders/909-snakes-and-ladders.js
@@ -8,19 +8,18 @@ var snakesAndLadders = function(board) {
     return [row,col];
   }
   const q = [1];
-  const seen = new Array(N*N).fill(0)
   let ans = 0
   let currLen = q.length, i = 0
-  seen[0] = 1
+  
   while(i<currLen) {
     const n = q.shift();
     if(n === N*N) return ans;
     for(let i = n+1; i <= Math.min(n+6, N*N); i++) {
       const [r, c] = getLoc(i);
       const next = board[r][c] === -1 ? i : board[r][c];
-      if(!seen[next-1]) {
+      if(board[r][c] !== 'X') {
         q.push(next);
-        seen[next-1] = 1;
+        board[r][c] = 'X'
       }
     }
       i++
