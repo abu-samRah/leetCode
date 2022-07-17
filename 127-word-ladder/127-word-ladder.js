b12d49008488fdfe476678455f33aef6ec8933fa
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-17 08:52:42 +0300

    Time: 2556 ms (6.73%), Space: 45.1 MB (97.67%) - LeetHub

--- 127-word-ladder/127-word-ladder.js
+++ 127-word-ladder/127-word-ladder.js
@@ -5,36 +5,32 @@
  * @return {number}
  */
 var ladderLength = function(beginWord, endWord, wordList) {
+   const queue = [beginWord]
+   let ans = 1
    const wordSet = new Set(wordList)
-    let queue = [beginWord];
-    let steps = 1;
+   let currLen = queue.length, i =0
+   
+   while(i<currLen){
+       const front = queue.shift()
+       
+       if(isGoal(front,endWord)) return ans
+       
+       for(let i=0; i<wordList.length; i++){
+           if(check(front,wordList[i],wordSet)){
+               queue.push(wordList[i])
+            wordSet.delete(wordList[i])
+           } 
+       }
+       i++
+       
+       if(i>=currLen){
+           ans++
+           currLen = queue.length
+           i = 0
+       }
+   }
     
-    while(queue.length) {
-        const next = [];
-        
-        // loop over each word in the queue
-        for(let word of queue) {
-            if(word === endWord) return steps;
-            
-            // loop over each char of the word 
-            for(let i = 0; i < word.length; i++) {
-                
-                // and replace the char with letters from [a - z]
-                for(let j = 0; j < 26; j++) {
-                    const newWord = word.slice(0, i) + String.fromCharCode(j + 97) + word.slice(i+1);
-                    
-                    // if the new word exist in the word list add it to the queue
-                    if(wordSet.has(newWord)) {
-                        next.push(newWord);
-                        wordSet.delete(newWord);
-                    }
-                }
-            }
-        }
-        queue = next
-        steps++;
-    }
-    return 0;  
+    return 0
 };
     
     
