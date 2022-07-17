6d08c11e239b13bea82b25f4096b66cd49033111
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-17 18:12:48 +0300

    Time: 102 ms (74.23%), Space: 49.4 MB (18.56%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.js
+++ 909-snakes-and-ladders/909-snakes-and-ladders.js
@@ -1,12 +1,6 @@
 var snakesAndLadders = function(board) {
   const N = board.length;
-  const getLoc = (pos) => {
-    let row = Math.floor((pos - 1) / N);
-    let col = (pos - 1) % N;
-    col = (row % 2) === 1 ? N - col - 1 : col;
-    row = N - row - 1;
-    return [row,col];
-  }
+  
   const q = [1];
   let ans = 0
   let currLen = q.length, i = 0
@@ -15,7 +9,7 @@ var snakesAndLadders = function(board) {
     const n = q.shift();
     if(n === N*N) return ans;
     for(let i = n+1; i <= Math.min(n+6, N*N); i++) {
-      const [r, c] = getLoc(i);
+      const [r, c] = getLoc(i,N);
       const next = board[r][c] === -1 ? i : board[r][c];
       if(board[r][c] !== 'X') {
         q.push(next);
@@ -32,3 +26,11 @@ var snakesAndLadders = function(board) {
   
   return -1;
 };
+
+const getLoc = (pos,N) => {
+    let row = Math.floor((pos - 1) / N);
+    let col = (pos - 1) % N;
+    col = (row % 2) === 1 ? N - col - 1 : col;
+    row = N - row - 1;
+    return [row,col];
+  }
