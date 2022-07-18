9cb8be1112d81cb7d94f7739080d73f26878d342
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-18 13:42:04 +0300

    Time: 451 ms (38.16%), Space: 58.1 MB (72.41%) - LeetHub

---
+++ 752-open-the-lock/752-open-the-lock.js
@@ -0,0 +1,56 @@
+/**
+ * @param {string[]} deadends
+ * @param {string} target
+ * @return {number}
+ */
+var openLock = function(deadends, target) {
+    
+    let ans = 0
+    const start = '0000'
+    let queue = [start]
+    let seen = {start:true}
+    const deadendsSet = new Set(deadends)
+    
+    if (deadendsSet.has(start)) return -1 
+    
+    let currLen = queue.length, i =0
+    
+    while(i<currLen){
+        const curr = queue.shift()
+        
+        if(isGoal(curr, target)) return ans
+        
+        for (let next of getNextStates(curr)) {
+            if(check(next,deadendsSet,seen)){
+                queue.push(next)
+                seen[next] = true
+            }
+        }
+        
+        i++
+        
+        if(i>=currLen){
+            ans++
+            currLen = queue.length
+            i = 0
+        }
+    }
+    
+    return -1
+};
+
+
+const isGoal = (curr, target) => target === curr
+
+const check = (next,deadendsSet,seen) => !deadendsSet.has(next) && !seen[next]
+
+function getNextStates(s = '0000') {
+  const ans= [];
+  
+  for (let i = 0; i < s.length; i++) {
+    ans.push(s.slice(0, i) + ((+s[i] + 1) % 10).toString() + s.slice(i + 1));
+    ans.push(s.slice(0, i) + ((+s[i] + 9) % 10).toString() + s.slice(i + 1));
+  }
+  
+  return ans;
+}
