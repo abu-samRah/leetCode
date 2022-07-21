ec5f27606dcc7c055dd8c2bbf54398f3618afa82
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-21 12:36:55 +0300

    Time: 289 ms (84.74%), Space: 61.3 MB (49.00%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
@@ -1,13 +1,12 @@
+/**
+ * @param {number[][]} heights
+ * @return {number}
+ */
 var minimumEffortPath = function(heights) {
-    const directions = [
-        [1, 0],
-        [0, 1],
-        [-1, 0],
-        [0, -1],
-    ];
     const row = heights.length;
     const col = heights[0].length;
-    const differences = [];
+
+   const differences = [];
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
             if (!differences[i]) {
@@ -17,88 +16,106 @@ var minimumEffortPath = function(heights) {
             }
         }
     }
-    differences[0][0] = 0;
-    const pq = new PriorityQueue();
-    pq.push([0, 0], 0);
-    while (pq.data.length > 0) {
-        const node = pq.shift();
-        const difference = node.priority;
-        const [x, y] = node.val;
-        directions.forEach(([dx, dy]) => {
-            const newX = x + dx;
-            const newY = y + dy;
-            if (newX >= 0 && newX < row && newY >= 0 && newY < col) {
+
+    differences[0][0] = 0
+    
+    const maxHeap = new PriorityQueue((a, b) => a[1] < b[1]);
+    
+    maxHeap.push([[0,0], 0]);
+    
+    while (!maxHeap.isEmpty()) {
+        // pop
+        const [ square, difference ] = maxHeap.pop();
+        const [x , y] = square
+        
+        //check nieghbours
+        const dx = [1,-1,0,0]
+        const dy = [0,0,1,-1]
+        for (let i=0; i<4; i++) {
+            const newX = x + dx[i];
+            const newY = y + dy[i];
+            if (check(newX , newY,heights)) {
                 const currentDiff = Math.abs(heights[newX][newY] - heights[x][y]);
                 const maxDiff = Math.max(currentDiff, differences[x][y]);
+                
                 if (differences[newX][newY] > maxDiff) {
                     differences[newX][newY] = maxDiff;
-                    pq.push([newX, newY], maxDiff);
+                    maxHeap.push([[newX, newY], maxDiff]);
                 } 
             }
-        });
+        }
+           
     }
+    
     return differences[row - 1][col - 1];
 };
 
-const swap = (arr, i, j) => {
-    const temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-};
+const check = (row,col,grid) => row>=0 && col >=0 && row<grid.length && col< grid[0].length 
 
-function Node(val, priority) {
-    this.val = val;
-    this.priority = priority;
+const top = 0;
+const parent = i => ((i + 1) >>> 1) - 1;
+const left = i => (i << 1) + 1;
+const right = i => (i + 1) << 1;
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this._heap = [];
+    this._comparator = comparator;
+  }
+  size() {
+    return this._heap.length;
+  }
+  isEmpty() {
+    return this.size() == 0;
+  }
+  peek() {
+    return this._heap[top];
+  }
+  push(...values) {
+    values.forEach(value => {
+      this._heap.push(value);
+      this._siftUp();
+    });
+    return this.size();
+  }
+  pop() {
+    const poppedValue = this.peek();
+    const bottom = this.size() - 1;
+    if (bottom > top) {
+      this._swap(top, bottom);
+    }
+    this._heap.pop();
+    this._siftDown();
+    return poppedValue;
+  }
+  replace(value) {
+    const replacedValue = this.peek();
+    this._heap[top] = value;
+    this._siftDown();
+    return replacedValue;
+  }
+  _greater(i, j) {
+    return this._comparator(this._heap[i], this._heap[j]);
+  }
+  _swap(i, j) {
+    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+  }
+  _siftUp() {
+    let node = this.size() - 1;
+    while (node > top && this._greater(node, parent(node))) {
+      this._swap(node, parent(node));
+      node = parent(node);
+    }
+  }
+  _siftDown() {
+    let node = top;
+    while (
+      (left(node) < this.size() && this._greater(left(node), node)) ||
+      (right(node) < this.size() && this._greater(right(node), node))
+    ) {
+      let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+      this._swap(node, maxChild);
+      node = maxChild;
+    }
+  }
 }
-
-function PriorityQueue() {
-    this.data = [];
-}
-
-PriorityQueue.prototype.push = function push(val, priority) {
-    const node = new Node(val, priority);
-    this.data.push(node);
-    let index = this.data.length - 1;
-    while (index > 0) {
-        const parentIndex = Math.floor((index - 1) / 2);
-        const parent = this.data[parentIndex];
-        if (parent.priority > node.priority) {
-            swap(this.data, parentIndex, index);
-            index = parentIndex;
-        } else {
-            break;
-        }
-    }
-};
-
-PriorityQueue.prototype.shift = function shift() {
-    const minNode = this.data[0] || {};
-    const lastNode = this.data.pop();
-    if (this.data.length < 1) {
-        return minNode;
-    }
-    this.data[0] = lastNode;
-    let index = 0;
-    while (index < this.data.length) {
-        const leftIndex = 2 * index + 1;
-        const rightIndex = 2 * index + 2;
-        const leftNode = this.data[leftIndex] || {};
-        const rightNode = this.data[rightIndex] || {};
-        let smallerIndex;
-        if (leftNode.priority < lastNode.priority) {
-            smallerIndex = leftIndex;
-        }
-        if (!smallerIndex && rightNode.priority < lastNode.priority) {
-            smallerIndex = rightIndex;
-        }
-        if (smallerIndex && rightNode.priority < leftNode.priority) {
-            smallerIndex = rightIndex;
-        }
-        if (!smallerIndex) {
-            break;
-        }
-        swap(this.data, index, smallerIndex);
-        index = smallerIndex;
-    }
-    return minNode;
-};
