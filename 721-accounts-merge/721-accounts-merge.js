bddfbc7b13830fe97c3adea49d1c367264033a6a
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-21 18:03:38 +0300

    Time: 260 ms (36.74%), Space: 78.3 MB (9.18%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.js
+++ 721-accounts-merge/721-accounts-merge.js
@@ -10,17 +10,19 @@ var accountsMerge = function(accounts) {
     
     for (let account of accounts) {
         let name = account[0];
-        
+
         for (let i=1;i<account.length;i++) {
             if (!graph[account[i]]) graph[account[i]] = new Set();
+            
             nameDict[account[i]] = name;
-            if (i != 1) {
+            
+            if (i !== 1) {
                 graph[account[1]].add(account[i]);
                 graph[account[i]].add(account[1]);
             }
         }
     }
-   // console.log(graph,nameDict)
+
     let res = [];
     let visited = new Set();
     
