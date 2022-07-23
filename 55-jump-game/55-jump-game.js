80b2f3b9d984b20dcf78108185a1fdacdc713c79
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-23 11:44:54 +0300

    Time: 358 ms (28.32%), Space: 48.6 MB (20.98%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -6,21 +6,18 @@ var canJump = function(nums) {
     const end = nums.length
     const queue = [0]
     const visited = new Array(end).fill(0)
-    
     if(nums.length<=1) return true
-    
-    visited[0] = 1
     while(queue.length){
         const curr = queue.shift()
-        let counter = 1
-        while(counter <= nums[curr]){
+        let counter = nums[curr]
+        while(counter > 0){
             const next = curr+counter
             if(next < end && !visited[next]) {
                 if(next === end-1) return true
                 queue.push(next)
                 visited[next] = 1
             } 
-            counter++
+            counter--
         }   
     }
     
