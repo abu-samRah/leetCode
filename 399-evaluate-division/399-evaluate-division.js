057c727314c75aa99bdcccf868b70ba342e4c122
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-27 09:35:12 +0300

    Time: 76 ms (68.28%), Space: 42 MB (77.31%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.js
+++ 399-evaluate-division/399-evaluate-division.js
@@ -4,53 +4,85 @@
  * @param {string[][]} queries
  * @return {number[]}
  */
+
 var calcEquation = function(equations, values, queries) {
-    const adjList = new Map()
+    const adjList = new Map();
     
     // Initialize the graph
-    for(let i=0; i<equations.length; i++){
-        adjList.set(equations[i][0], [])
-        adjList.set(equations[i][1], [])
+    for (let i = 0; i < equations.length; i++) {
+        adjList.set(equations[i][0], []);
+        adjList.set(equations[i][1], []);
     }
-    
-     // Build the graph
-    for(let i=0; i<equations.length; i++){
-        const u = equations[i][0]
-        const v = equations[i][1]
-        const weight = values[i]
-        
-        adjList.get(u).push([v, weight])
-        adjList.get(v).push([u, 1 / weight])
-    }
-    
-    const res = []
-    
-    for(let i=0; i<queries.length; i++){
-        const src = queries[i][0]
-        const dst = queries[i][1]
-        const seen = new Set();
-        
-        const val = dfs(adjList,src,src,dst,1,seen)
-        
-        res.push(val? val : -1)
-    }
-    
-    return res
-};
 
-const dfs = (adjList,src,curr,dst,quotient,seen) =>{
-    if(!adjList.has(src) || !adjList.has(dst)) return -1
-    if(src === dst) return 1
-    if(curr === dst) return quotient
-    seen.add(curr)
+    // Build the graph
+    for (let i = 0; i < equations.length; i++) {
+        const u = equations[i][0];
+        const v = equations[i][1];
+        const weight = values[i];
+        
+        // u to v
+        adjList.get(u).push([v,weight]);
+        
+        // v to u
+        adjList.get(v).push([u, 1/weight]);
+    }
+    
+    // Initialize results array
+    const res = [];
+    
+    for (let i = 0; i < queries.length; i++) {
+        // divisor
+        const src = queries[i][0];
+        
+        // dividend
+        const dest = queries[i][1];
+        const seen = new Set();
+        const val = dfs(adjList, src, src, dest, 1, seen);
+        
+        
+        if (val === false) {
+            res.push(-1);
+        } else {
+            res.push(val);
+        }
+    }
+    
+    return res;
+}
+// In the case that there are other multiple valid paths from source to destination, ultimately, the quotient is the same
+// returning the first then works.
+var dfs = function(adjList, src, curr, dest, quotient, seen) {
+    //destination or source is non existent
+    if (!adjList.has(dest) || !adjList.has(src)) {
+        return -1;
+    }
+    
+    //source is same as destination i.e. self-loop.
+    if (src === dest) {
+        return 1;
+    }
+    
+    // Reached destination
+    if (curr === dest) {
+        return quotient;
+    }
+    
+    seen.add(curr);
     
     const neighbors = adjList.get(curr);
     
-    for (let i=0; i<neighbors.length; i++){
-        if(seen.has(neighbors[i][0])) continue
-        const val = dfs(adjList, src, neighbors[i][0], dst, quotient * neighbors[i][1], seen);
-        if (val !== false) return val
+    for (let i = 0; i < neighbors.length; i++) {
+        if (seen.has(neighbors[i][0])) {
+            continue;
+        }
+        const val = dfs(adjList, src, neighbors[i][0], dest, quotient * neighbors[i][1], seen);
+        // if any value was found we return because
+        // the only other cases would be having a cycle, or reaching our destination or path.
+        if (val !== false) return val;
     }
     
-    return false
+    // We only return false if we've searched all neighbors and there's no path to destination
+    // however we know that src and dest are in adjList
+    // thus it may just be disconnected, but this may return from different levels of recursion.
+    return false;
 }
