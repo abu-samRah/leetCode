13f012e66f8daf363e54bffc1f5707f919c68f4a
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-29 19:52:12 +0300

    Time: 97 ms (59.19%), Space: 43.3 MB (74.78%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.js
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.js
@@ -5,12 +5,8 @@
 var titleToNumber = function(s) {
     let columnNumber = 0;
     for(let i = 0; i < s.length; i++){
-        // get the last ith character
         let char = s[(s.length - 1) - i];
         
-        // subtracting from 64 (not 65, ascii of A) to get the value directly 
-        // instead of ascii index
-        // ex: A = 1 (65 - 64 = 1)
         columnNumber += Math.pow(26, i) * (char.charCodeAt(0) - 64);
         
     } 
