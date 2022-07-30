f9017a61fb63ad96c67b8ec134474392e5e3e5a1
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-07-30 13:37:12 +0300

    Time: 154 ms (57.51%), Space: 52.3 MB (29.81%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
@@ -4,21 +4,12 @@
  */
 var allPathsSourceTarget = function(graph) {
     const ans = []
-    const visited = {}
     const n = graph.length - 1
     
     const dfs = (i, path) =>{
-        if(i === n) {
-            ans.push(path) 
-            return
-        }
-        
-        visited[i] = true
-     
-        for(let neigh of graph[i]){
-           
-                dfs(neigh, [...path,neigh])
-        }
+        if(i === n) ans.push(path) 
+        for(let neigh of graph[i])
+            dfs(neigh, [...path,neigh])
     }
     
     dfs(0,[0])
