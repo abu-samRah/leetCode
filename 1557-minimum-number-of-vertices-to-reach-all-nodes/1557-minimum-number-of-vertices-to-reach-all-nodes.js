143b16f5b3b192b77ac395b2b2f55a40efe5a370
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-01 20:16:24 +0300

    Time: 174 ms (96.68%), Space: 68.5 MB (88.63%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.js
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.js
@@ -4,14 +4,17 @@
  * @return {number[]}
  */
 var findSmallestSetOfVertices = function(n, edges) {
-    const graph = [...Array(n)].map(e=>[])
-    const ans = []
-    
-    for(let i=0; i<edges.length;i++)
-        graph[edges[i][1]].push(edges[i][0])
 
-    for(let i=0; i<graph.length;i++)
-        !graph[i].length && ans.push(i)
+    const ans = []
+    const graph = new Array(n).fill(0)
+    
+    for(let i=0; i<edges.length;i++){
+        graph[edges[i][1]] = true
+    }
+       
+
+    for(let i=0; i<n;i++)
+        !graph[i] && ans.push(i)
     
     return ans
 };
