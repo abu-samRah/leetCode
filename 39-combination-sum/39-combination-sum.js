ddded911ec58d43c6ce098c3457f156425b8158c
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-02 14:37:42 +0300

    Time: 158 ms (23.73%), Space: 49.3 MB (24.39%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+   
+    const getCombinations = (sum,out,val) =>{
+        if(sum === target ) {
+            
+             out.push(val)
+            return
+        }
+        if(sum>target) return
+        
+        for(let i=0; i<candidates.length;i++)
+            if(!val.length || candidates[i]>=val[val.length-1])
+                getCombinations(sum+candidates[i], out , [...val,candidates[i]])
+           
+        
+        
+        return out
+    }
+    return getCombinations(0,[],[])
+    
+};
+
+/*
+getCombinations(candidates,target,sum,out)
+for(let i = 0)
+*/
