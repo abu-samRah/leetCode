11422c6a3489767b7838818f44e43ad571273560
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-04 08:42:34 +0300

    Time: 102 ms (33.33%), Space: 42.3 MB (33.33%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.js
+++ 858-mirror-reflection/858-mirror-reflection.js
@@ -3,17 +3,17 @@
  * @param {number} q
  * @return {number}
  */
-
+const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
 
 const mirrorReflection = (p, q) => {
-    while (isEven(p) && isEven(q)) {
-        p /= 2;
-        q /= 2;
+    if(isEven(p) && isEven(q)) {
+     let g = gcd(p, q);
+     p /= g;
+     q /= g;
     }
-    
-    if(isEven(p)) return 2
-    if(isEven(q)) return 0
-    return 1
+    if(isOdd(p) && isEven(q)) return 0
+    if(isOdd(p) && isOdd(q))  return 1
+    if(isOdd(q) && isEven(p)) return 2
 };
 
 const isEven = (a) => a % 2 === 0
