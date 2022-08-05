4ceef89e4080a382add4da82d9ce691dd62adeb6
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-05 10:12:46 +0300

    Time: 76 ms (79.76%), Space: 42.3 MB (76.87%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.js
+++ 377-combination-sum-iv/377-combination-sum-iv.js
@@ -3,18 +3,16 @@ var combinationSum4 = function(nums, target) {
     const memo = {};
     
     function getCombinations(target) {
-        
         if (target === 0) return 1;
-        if (target < 0) return 0;
         if (!isNaN(memo[target])) return memo[target]
         let count = 0;
         
         for (let i = 0; i < n; i++) {
             const num = nums[i];
-            
+            if (num <= target) {
                 const amountLeft = target - num;
                 count += getCombinations(amountLeft);
-            
+            }
         }
         
         memo[target]  = count;
