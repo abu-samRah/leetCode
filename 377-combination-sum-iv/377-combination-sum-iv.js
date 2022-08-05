4ac2a0d3985ade085885cb80c33bd075890ca1ca
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-05 10:04:14 +0300

    Time: 115 ms (21.93%), Space: 42.3 MB (76.87%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.js
@@ -0,0 +1,26 @@
+var combinationSum4 = function(nums, target) {
+    const n = nums.length;
+    const memo = new Map();
+    
+    return topDown(target);
+    
+    function topDown(target) {
+        // base cases
+        if (target == 0) return 1;
+        if (target < 0) return 0;
+        if (memo.has(target)) return memo.get(target);
+         
+        let count = 0;
+        
+        for (let i = 0; i < n; i++) {
+            const num = nums[i];
+            if (num <= target) {
+                const amountLeft = target - num;
+                count += topDown(amountLeft);
+            }
+        }
+        
+        memo.set(target, count);
+        return count;
+    }
+};
