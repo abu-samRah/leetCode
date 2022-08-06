59886fd1c59114cec7bd4ca3254f8bf2748592c3
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-06 11:01:19 +0300

    Time: 105 ms (54.72%), Space: 48.3 MB (10.21%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    if (!candidates || candidates.length == 0) return [];
+    let res = [];
+    candidates.sort((a,b) => a-b);
+    var helper = function(curSum, cur, index){
+        if (curSum == target){
+            res.push([...cur]);
+            return;
+        }
+        for(let i = index; i < candidates.length; i++){
+            if (i != index && candidates[i] == candidates[i-1]) continue; //already return, go next loop(not recursion)
+            if (curSum > target) return;
+            helper(curSum+candidates[i], [...cur,candidates[i]], i+1);
+        }
+    }
+    helper(0, [], 0);
+    return res;
+};
