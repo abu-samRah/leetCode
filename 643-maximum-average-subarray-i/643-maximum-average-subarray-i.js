f7e9f492a0feb615a02d3610c3f11767dd602abe
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-07 08:39:09 +0300

    Time: 212 ms (14.54%), Space: 54.9 MB (51.44%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.js
@@ -7,13 +7,11 @@ var findMaxAverage = function(nums, k) {
     let max = Number.NEGATIVE_INFINITY
     let sum = 0
     for(let i=0; i< nums.length+1; i++){
-        if(i < k) {
-            sum += nums[i]
-        }else if(i >= k){
+        if(i >= k){
             max = Math.max(max, sum/k)
             sum -= nums[i-k]
-            sum += nums[i]
         }
+        sum += nums[i]
     }
     return max
 };
