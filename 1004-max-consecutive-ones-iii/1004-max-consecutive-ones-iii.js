b9d4300ae815072efa02dd02811d24d393ca7b06
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-08 09:20:41 +0300

    Time: 133 ms (20.94%), Space: 47.4 MB (15.47%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -5,7 +5,6 @@
  */
 var longestOnes = function(arr, k) {
   let maxLength = 0
-  let maxRepeatOnes = 0
   let frequencyMap = {};
 
   let windowStart = 0 
@@ -14,9 +13,7 @@ var longestOnes = function(arr, k) {
       
     frequencyMap[rightNum] = (frequencyMap[rightNum] || 0 ) +1
     
-    maxRepeatOnes = frequencyMap[1];
-    
-    if ((windowEnd - windowStart + 1 - maxRepeatOnes) > k) {
+    if ((windowEnd - windowStart + 1 - frequencyMap[1]) > k) {
       let leftNum = arr[windowStart];
       frequencyMap[leftNum] -= 1;
       windowStart += 1;
@@ -24,6 +21,7 @@ var longestOnes = function(arr, k) {
 
     maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
   }
+
    
   return isNaN(frequencyMap[1])? k :maxLength;
 };
