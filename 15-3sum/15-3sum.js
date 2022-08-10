8e25cea3dea19d0bc76b2d14186cdfddf3370b43
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-10 08:09:47 +0300

    Time: 241 ms (43.56%), Space: 53.1 MB (40.88%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,7 +3,7 @@
  * @return {number[][]}
  */
 var threeSum = function(arr) {
-   arr.sort((a, b) => a - b);
+  arr.sort((a, b) => a - b);
   const triplets = [];
   for (let i = 0; i < arr.length; i++) {
     if (i > 0 && arr[i] === arr[i - 1]) { // skip same element to avoid duplicate triplets
@@ -20,20 +20,20 @@ function search_pair(arr, target_sum, left, triplets) {
   let right = arr.length - 1;
   while (left < right) {
     const current_sum = arr[left] + arr[right];
-    if (current_sum === target_sum) { // found the triplet
+    if (current_sum === target_sum) { 
       triplets.push([-target_sum, arr[left], arr[right]]);
       left += 1;
       right -= 1;
-      while (left < right && arr[left] === arr[left - 1]) {
-        left += 1; // skip same element to avoid duplicate triplets
+      while ( arr[left] === arr[left - 1]) {
+        left += 1; 
       }
-      while (left < right && arr[right] === arr[right + 1]) {
-        right -= 1; // skip same element to avoid duplicate triplets
+      while ( arr[right] === arr[right + 1]) {
+        right -= 1;
       }
     } else if (target_sum > current_sum) {
-      left += 1; // we need a pair with a bigger sum
+      left += 1; 
     } else {
-      right -= 1; // we need a pair with a smaller sum
+      right -= 1; 
     }
   }
 }
