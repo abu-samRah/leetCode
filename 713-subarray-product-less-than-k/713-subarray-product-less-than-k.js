ba858bf1af7c307d3854ba9b0519c850b8e6d313
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-10 09:39:30 +0300

    Time: 824 ms (21.92%), Space: 47.9 MB (48.48%) - LeetHub

---
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(arr, target) {
+    let result = 0,
+    product = 1,
+    left = 0;
+  for (right = 0; right < arr.length; right++) {
+    product *= arr[right];
+    while ((product >= target && left <= right)) {
+      product /= arr[left];
+      left += 1;
+    } 
+    
+    for (let i = right; i > left - 1; i--) {
+      result++
+    }
+  }
+  
+  return result;
+    
+};
