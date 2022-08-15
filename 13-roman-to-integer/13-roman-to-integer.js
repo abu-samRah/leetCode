eb634859330863c7246adb2050ff04d61bbe8656
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-15 17:20:34 +0300

    Time: 225 ms (35.78%), Space: 47.1 MB (67.82%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+        const romans = {
+          I: 1,
+          V: 5,
+          X: 10,
+          L: 50,
+          C: 100,
+          D: 500,
+          M: 1000,
+        };
+    
+    let res = 0
+    
+    for (let i =0; i<s.length ; i++){
+        if(romans[s[i]] < romans[s[i+1]]){
+            res -= romans[s[i]]
+        }else{
+            res += romans[s[i]]
+        }
+    }
+    
+    return res
+};
