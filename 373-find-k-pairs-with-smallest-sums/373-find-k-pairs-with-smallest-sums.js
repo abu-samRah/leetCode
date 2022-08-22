992e5979f1e08a3ba8d90b16a67022b6d2cd2c5f
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-22 09:24:40 +0300

    Time: 623 ms (31.70%), Space: 89.5 MB (40.24%) - LeetHub

---
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.js
@@ -0,0 +1,123 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+
+
+class BinaryHeap {
+  constructor(comparator = (a, b) => {
+  	return (a < b) ? -1 : (a === b ? 0 : 1);
+  }) {
+    this.heap = [];
+    this.comparator = comparator;
+  }
+  size() {
+    return this.heap.length;
+  }
+
+  getLeftIndex(index) {
+  	return 2 * index + 1;
+  }
+  getRightIndex(index) {
+  	return 2 * index + 2;
+  }
+  getParentIndex(index) {
+  	return Math.floor((index - 1) / 2);
+  }
+    
+    insert(data) {
+  	if (data === undefined || data === null) {
+    	return false;
+    }
+    this.heap.push(data);
+    this.bubbleUp(this.heap.length - 1);
+    return true;
+  }
+  bubbleUp(index) {    
+    while (index > 0) {
+      let curr = this.heap[index];
+      let parentIndex = this.getParentIndex(index);
+      let parent = this.heap[parentIndex];
+      
+      let compare = this.comparator(parent, curr);
+      if (compare < 0 || compare === 0) {
+        break;
+      }
+      
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
+  }
+  swap(a, b) {
+  	[this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+    
+    peak() {
+  	return this.size() > 0 ? this.heap[0] : undefined;
+  }
+    
+    extract() {
+  	if (this.size() === 0) {
+    	return undefined;
+    }
+    
+    if (this.size() === 1) {
+    	return this.heap.shift();
+    }
+    
+    const value = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.sinkDown(0);
+    return value;
+  }
+  sinkDown(currIndex) {
+    let left = this.getLeftIndex(currIndex);
+    let right = this.getRightIndex(currIndex);
+    let parentIndex = currIndex;
+    
+    if (left < this.size() && this.comparator(this.heap[left], this.heap[parentIndex]) < 0) {
+      parentIndex = left;
+    }
+    
+    if (right < this.size() && this.comparator(this.heap[right], this.heap[parentIndex]) < 0) {
+      parentIndex = right;
+    }
+    
+    if (parentIndex !== currIndex) {
+    	this.swap(parentIndex, currIndex);
+      this.sinkDown(parentIndex);
+    }
+  }
+    
+    values(){
+        return this.heap
+    }
+}
+
+var kSmallestPairs = function(nums1, nums2, k) {
+    const maxHeap = new BinaryHeap((a,b) => !b ? a : b[0] - a[0])
+    
+    for (let i = 0; i <  Math.min(k, nums1.length); i++) {
+        for (let j = 0; j < Math.min(k, nums2.length); j++) {
+          if (maxHeap.size() < k) {
+            maxHeap.insert([nums1[i] + nums2[j], i, j]);
+          } else {
+            if (nums1[i] + nums2[j] > maxHeap.peak()[0]) {
+              break;
+            } else { 
+              maxHeap.extract();
+              maxHeap.insert([nums1[i] + nums2[j], i, j]);
+            }
+          }
+        }
+    }
+    
+  const result = []
+  maxHeap.values().forEach((a) => {
+    result.push([nums1[a[1]], nums2[a[2]]]);
+  });
+
+  return result;
+};
