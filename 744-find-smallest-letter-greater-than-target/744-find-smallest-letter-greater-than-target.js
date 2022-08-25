236d1a35625da2fea3e8c6bd716e9cc7c28b5055
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-25 09:41:19 +0300

    Time: 109 ms (29.45%), Space: 43.8 MB (46.40%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
@@ -3,15 +3,16 @@
  * @param {character} target
  * @return {character}
  */
-var nextGreatestLetter = function(letters, target) {
-    let high = letters.length
+var nextGreatestLetter = function(nums, target) {
     let low = 0
-    
-    while(low < high){
-        const mid = low + Math.floor((high - low)/2)
-        if(letters[mid] > target) high = mid
-        else low = mid + 1
-    }
-    
-    return letters[high] ?? letters[0]
+  let high = nums.length -1
+  
+  while(low<=high){
+      let mid =  low + Math.floor((high - low) /2)
+      if(nums[mid] >target)  high = mid - 1
+      else  low = mid+1 
+      
+  }
+  
+  return nums[low] ? nums[low] : nums[0]
 };
