392ceb83dcd55ecccadc6f58b481fc2bb88617de
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-30 19:05:30 +0300

    Time: 165 ms (32.11%), Space: 50.5 MB (7.74%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.js
+++ 986-interval-list-intersections/986-interval-list-intersections.js
@@ -2,25 +2,34 @@
  * @param {number[][]} firstList
  * @param {number[][]} secondList
  * @return {number[][]}
+ 
+ [[0,2],[5,10],[13,23],[24,25]] [[1,5],[8,12],[15,24],[25,26]]
+ 
+ [0,2]  [1,5]
+ s1 e1  s2 e2
+ s1>=s2 && s1<=e2  || s2>=s1 && s2<=e1
+ 
+ 0>=1 && 0<=5  ||  1>=0 && 1<=2   res = [[1,2]]
+ 
+ e1<e2 i++ 
+ j++ 
  */
 var intervalIntersection = function(firstList, secondList) {
-    let i =0
-    let j = 0
-    const res = []
-    
-    while(i< firstList.length && j< secondList.length){
-        const a_overlap = firstList[i][0] >= secondList[j][0] && firstList[i][0]  <= secondList[j][1]
-        const b_overlap = secondList[j][0] >= firstList[i][0] && secondList[j][0] <= firstList[i][1]
+    let res = []
+    let i =0 
+    let j =0
+
+    while(i<firstList.length && j<secondList.length){
+        const [s1,e1] = firstList[i]
+        const [s2,e2] = secondList[j]
         
-        if(a_overlap || b_overlap){
-            res.push([Math.max(firstList[i][0], secondList[j][0]) , Math.min(firstList[i][1],secondList[j][1])])
+        if(s1>=s2 && s1<=e2  || s2>=s1 && s2<=e1){
+            res.push([Math.max(s1,s2),Math.min(e1,e2)])
         }
         
-        if(firstList[i][1] < secondList[j][1]){
-            i++
-        }else{
-            j++
-        }
+        if(e1<e2) i++ 
+        else j++ 
     }
+    
     return res
 };
