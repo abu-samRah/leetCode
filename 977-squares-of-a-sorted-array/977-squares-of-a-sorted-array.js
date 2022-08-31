633e7bfb9155a33f3a4dc0d790d89fc48177fe0d
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-08-31 08:08:54 +0300

    Time: 155 ms (55.11%), Space: 48.8 MB (40.29%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -2,35 +2,24 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(arr) {
-   let   squares = []
-  // TODO: Write your code here
-
-  let j = 0
-  while(j<arr.length){
-    if(arr[j] >= 0) break
-    j++
-  }
-
-  let i = j-1
-
-  while(i>=0 && j<arr.length){
-    if(Math.abs(arr[i]) < Math.abs([arr[j]])){
-      squares.push(Math.pow(arr[i],2))
-      i--
-    }else{
-      squares.push(Math.pow(arr[j],2))
-      j++
+var sortedSquares = function(nums) {
+    const squares = new Array(nums.length)
+    let top = nums.length -1
+    
+    let j = top
+    let i=0
+    while(i<=j){
+        const left = Math.pow(nums[i],2)
+        const right = Math.pow(nums[j],2)
+        if(left>=right){
+            squares[top] = left
+            i++
+        }else{
+            squares[top] = right
+            j--
+        }
+        top--
     }
-  }
-  while(i>=0){
-    squares.push(Math.pow(arr[i],2))
-    i--
-  }
-
-   while(j<arr.length){
-     squares.push(Math.pow(arr[j],2))
-      j++
-  }
-  return squares;
+    
+    return squares
 };
