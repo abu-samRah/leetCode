80a1b8a94eae58284738988e0005240026dbcdf9
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-01 08:06:03 +0300

    Time: 119 ms (53.60%), Space: 47.3 MB (28.27%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -3,77 +3,129 @@
  * @param {number} k
  * @return {number[]}
  */
-class MinHeap {
-        
-        constructor(capacity) {
-            this.capacity = capacity;
-            this.value = [];
-        }
-        
-        add(val) {
-            this.value.push(val);
-            this.bubbleUp(this.value.length-1);
-            if(this.value.length > this.capacity) this.remove();
-        }
-        
-        remove() {
-            this.swap(0, this.value.length-1);
-            const min = this.value.pop();
-            this.trickleDown(0);  
-            return min;
-        }
-        
-        bubbleUp(idx) {
-            const parent = Math.floor((idx-1)/2);
-            let max = idx;
-            
-            if(parent >= 0 && this.value[parent][0] > this.value[max][0]) max = parent;
-            
-            if(max !== idx) {
-                this.swap(max, idx);
-                this.bubbleUp(max);
-            }
-        }
-        
-        trickleDown(idx) {
-            const leftChild = 2 * idx + 1;
-            const rightChild = 2 * idx + 2;
-            let min = idx;
-            
-            if(leftChild < this.value.length && this.value[leftChild][0] < this.value[min][0]) min = leftChild;
-            if(rightChild < this.value.length && this.value[rightChild][0] < this.value[min][0]) min = rightChild;
-            
-            if(min !== idx) {
-                this.swap(min, idx);
-                this.trickleDown(min);
-            }
-        }
-        
-        swap(i, j) {
-            [this.value[i], this.value[j]] = [this.value[j], this.value[i]];
-        }
-    }
-
-var topKFrequent = function(nums, k) {
-    let numFrequencyMap = {};
-    nums.forEach((num) => {
-        if (!(num in numFrequencyMap)) {
-          numFrequencyMap[num] = 1;
-        } else {
-          numFrequencyMap[num]++;
-        }
-     });
-    
-    const minHeap = new MinHeap(k)
-    
-      Object.keys(numFrequencyMap).forEach((num) => {
-        minHeap.add([numFrequencyMap[num], num]);
-  });
-     const topNumbers = [];
-  while (k > 0) {
-    topNumbers.push(minHeap.remove()[1]);
-      k--
+ class BinaryHeap {
+  constructor(comparator = (a, b) => {
+  	return (a < b) ? -1 : (a === b ? 0 : 1);
+  }) {
+    this.heap = [];
+    this.comparator = comparator;
+  }
+  size() {
+    return this.heap.length;
   }
 
-  return topNumbers;
+  getLeftIndex(index) {
+  	return 2 * index + 1;
+  }
+  getRightIndex(index) {
+  	return 2 * index + 2;
+  }
+  getParentIndex(index) {
+  	return Math.floor((index - 1) / 2);
+  }
+    
+    insert(data) {
+  	if (data === undefined || data === null) {
+    	return false;
+    }
+    this.heap.push(data);
+    this.bubbleUp(this.heap.length - 1);
+    return true;
+  }
+  bubbleUp(index) {    
+    while (index > 0) {
+      let curr = this.heap[index];
+      let parentIndex = this.getParentIndex(index);
+      let parent = this.heap[parentIndex];
+      
+      let compare = this.comparator(parent, curr);
+      if (compare < 0 || compare === 0) {
+        break;
+      }
+      
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
+  }
+  swap(a, b) {
+  	[this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+    
+    peak() {
+  	return this.size() > 0 ? this.heap[0] : undefined;
+  }
+    
+    extract() {
+  	if (this.size() === 0) {
+    	return undefined;
+    }
+    
+    if (this.size() === 1) {
+    	return this.heap.shift();
+    }
+    
+    const value = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.sinkDown(0);
+    return value;
+  }
+  sinkDown(currIndex) {
+    let left = this.getLeftIndex(currIndex);
+    let right = this.getRightIndex(currIndex);
+    let parentIndex = currIndex;
+    
+    if (left < this.size() && this.comparator(this.heap[left], this.heap[parentIndex]) < 0) {
+      parentIndex = left;
+    }
+    
+    if (right < this.size() && this.comparator(this.heap[right], this.heap[parentIndex]) < 0) {
+      parentIndex = right;
+    }
+    
+    if (parentIndex !== currIndex) {
+    	this.swap(parentIndex, currIndex);
+      this.sinkDown(parentIndex);
+    }
+  }
+    
+    values(){
+        return this.heap
+    }
+}
+
+/*
+[1,1,1,2,2,3]
+
+map = {1:3, 2:2, 3:1,4:5}
+        3
+       /
+      4
+*/
+
+var topKFrequent = function(nums, k) {
+    const map = {}
+    
+    for(let n of nums) map[n] = (map[n] || 0) + 1
+    
+    const minHeap = new BinaryHeap((a,b) => map[a] - map[b])
+    
+    const values = Object.keys(map)
+    
+    for(let i=0; i<values.length; i++){
+        if(minHeap.size() < k ) minHeap.insert(values[i])
+        else{
+            if(map[minHeap.peak()] < map[values[i]]){
+                minHeap.extract()
+                minHeap.insert(values[i])
+            }
+        }
+    }
+    
+    const res = []
+    while(minHeap.size()){
+        res.push( minHeap.extract() )
+    }
+    
+    return res
+   
 };
