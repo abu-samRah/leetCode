32d3fe1964995364bc1a06ec29591ae6b75701a7
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-01 09:20:51 +0300

    Time: 163 ms (15.06%), Space: 46.8 MB (34.62%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.js
+++ 767-reorganize-string/767-reorganize-string.js
@@ -1,101 +1,111 @@
-/**
- * @param {string} s
- * @return {string}
- */
-/** 
- *  custom Heap class
- */
-class Heap {
-	constructor(comparator) {
-		this.size = 0;
-		this.values = [];
-		this.comparator = comparator;
-	}
+class BinaryHeap {
+  constructor(comparator = (a, b) => {
+  	return (a < b) ? -1 : (a === b ? 0 : 1);
+  }) {
+    this.heap = [];
+    this.comparator = comparator;
+  }
+  size() {
+    return this.heap.length;
+  }
 
-	add(val) {
-		this.values.push(val);
-		this.size ++;
-		this.bubbleUp();
-	}
-
-	peek() {
-		return this.values[0] || null;
-	}
+  getLeftIndex(index) {
+  	return 2 * index + 1;
+  }
+  getRightIndex(index) {
+  	return 2 * index + 2;
+  }
+  getParentIndex(index) {
+  	return Math.floor((index - 1) / 2);
+  }
     
-    length() {
-		return this.values.length;
-	}
-
-	poll() {
-		const max = this.values[0];
-		const end = this.values.pop();
-		this.size --;
-		if (this.values.length) {
-			this.values[0] = end;
-			this.bubbleDown();
-		}
-		return max;
-	}
-
-	bubbleUp() {
-		let index = this.values.length - 1;
-		let parent = Math.floor((index - 1) / 2);
-
-		while (this.comparator(this.values[index], this.values[parent]) < 0) {
-			[this.values[parent], this.values[index]] = [this.values[index], this.values[parent]];
-			index = parent;
-			parent = Math.floor((index - 1) / 2);
-		}
-	}
-
-	bubbleDown() {
-		let index = 0, length = this.values.length;
-
-		while (true) {
-			let left = null,
-				right = null,
-				swap = null,
-				leftIndex = index * 2 + 1,
-				rightIndex = index * 2 + 2;
-
-			if (leftIndex < length) {
-				left = this.values[leftIndex];
-				if (this.comparator(left, this.values[index]) < 0) swap = leftIndex;
-			}
-
-			if (rightIndex < length) {
-				right = this.values[rightIndex];
-				if ((swap !== null && this.comparator(right, left) < 0) || (swap === null && this.comparator(right, this.values[index]))) {
-					swap = rightIndex;
-				}
-			}
-			if (swap === null) break;
-
-			[this.values[index], this.values[swap]] = [this.values[swap], this.values[index]];
-			index = swap;
-		}
-	}
+    insert(data) {
+  	if (data === undefined || data === null) {
+    	return false;
+    }
+    this.heap.push(data);
+    this.bubbleUp(this.heap.length - 1);
+    return true;
+  }
+  bubbleUp(index) {    
+    while (index > 0) {
+      let curr = this.heap[index];
+      let parentIndex = this.getParentIndex(index);
+      let parent = this.heap[parentIndex];
+      
+      let compare = this.comparator(parent, curr);
+      if (compare < 0 || compare === 0) {
+        break;
+      }
+      
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
+  }
+  swap(a, b) {
+  	[this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+    
+    peak() {
+  	return this.size() > 0 ? this.heap[0] : undefined;
+  }
+    
+    extract() {
+  	if (this.size() === 0) {
+    	return undefined;
+    }
+    
+    if (this.size() === 1) {
+    	return this.heap.shift();
+    }
+    
+    const value = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.sinkDown(0);
+    return value;
+  }
+  sinkDown(currIndex) {
+    let left = this.getLeftIndex(currIndex);
+    let right = this.getRightIndex(currIndex);
+    let parentIndex = currIndex;
+    
+    if (left < this.size() && this.comparator(this.heap[left], this.heap[parentIndex]) < 0) {
+      parentIndex = left;
+    }
+    
+    if (right < this.size() && this.comparator(this.heap[right], this.heap[parentIndex]) < 0) {
+      parentIndex = right;
+    }
+    
+    if (parentIndex !== currIndex) {
+    	this.swap(parentIndex, currIndex);
+      this.sinkDown(parentIndex);
+    }
+  }
+    
+    values(){
+        return this.heap
+    }
 }
-
 
 var reorganizeString = function(s) {
     const freqMap = {}
     for(const char of s) freqMap[char] = (freqMap[char] || 0) +1
     
-    const maxHeap = new Heap((a,b) => !b ? a : b[0] - a[0])
+    const maxHeap = new BinaryHeap((a,b) => b[0] - a[0])
     Object.keys(freqMap).forEach((char) => {
-     maxHeap.add([freqMap[char], char]);
+     maxHeap.insert([freqMap[char], char]);
     });
     
   let previousChar = null
   let previousFrequency = 0
   let resultString = [];
     
-  while (maxHeap.length() > 0) {
-    const [frequency, char] = maxHeap.poll();
+  while (maxHeap.size() > 0) {
+    const [frequency, char] = maxHeap.extract();
   
     if (previousChar !== null && previousFrequency > 0) {
-      maxHeap.add([previousFrequency, previousChar]);
+      maxHeap.insert([previousFrequency, previousChar]);
     }
     
     resultString.push(char);
