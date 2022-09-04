4c5cdd0a600331c5775f9d4530e14acaa72954ef
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-04 09:16:48 +0300

    Time: 112 ms (34.80%), Space: 44.7 MB (19.21%) - LeetHub

--- 202-happy-number/202-happy-number.js
+++ 202-happy-number/202-happy-number.js
@@ -1,8 +1,6 @@
 /**
  * @param {number} n
  * @return {boolean}
-
-
  */
 
 var isHappy = function(n) {
@@ -13,8 +11,12 @@ var isHappy = function(n) {
     }
     return n === 1 ? true : false;
 };
-function sumOfSquares(numString) {
-    return numString.toString().split('').reduce(function(sum, num) {
-        return sum + Math.pow(num, 2);
-    }, 0);
+function sumOfSquares(num) {
+    let sum = 0;
+    while ((num > 0)) {
+        const digit = num % 10;
+        sum += digit * digit;
+        num = Math.floor(num / 10);
+    }
+    return sum;
 }
