f0f0eccd9f1c54822bb20354941450a75d020102
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-06 14:08:21 +0300

    Time: 109 ms (81.22%), Space: 47.1 MB (88.17%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.js
+++ 437-path-sum-iii/437-path-sum-iii.js
@@ -12,25 +12,45 @@
  * @return {number}
  */
 var pathSum = function(root, targetSum) {
-    let ans = 0
-    const dfs = (node, sum, path) =>{
-       
-        if(!node) return 0
-        path.push(node.val)
-        let pathSum = 0;
-        
-         for (let i = path.length - 1; i >= 0; i--) {
-            pathSum += path[i];
-            if (pathSum === sum) {
-              ans += 1;
-            }
-         }
-       
-        dfs(node.left  ,sum  ,[...path]) 
-        dfs(node.right ,sum ,[...path])
-       
-    }
-    
-    dfs(root,targetSum,[])
-    return ans
+    // A map that stores the number of times a prefix sum has occurred so far.
+  const map = new Map();
+
+  return count_paths_prefix_sum(root, targetSum, map, 0);
+}
+
+function count_paths_prefix_sum(node, targetSum, map, currentPathSum) {
+  if (!node) return 0;
+
+  // The number of paths that have the required sum.
+  let pathCount = 0;
+
+  // 'currentPathSum' is the prefix sum, i.e., sum of all node values from root to current node.
+  currentPathSum += node.val;
+
+  // This is the base case. If the current sum is equal to the target sum, we have found a path from root to
+  // the current node having the required sum. Hence, we increment the path count by 1.
+  if (targetSum === currentPathSum) pathCount++;
+
+  //'currentPathSum' is the path sum from root to the current node. If within this path, there is a
+  // valid solution, then there must be an 'oldPathSum' such that:
+  // => currentPathSum - oldPathSum = targetSum
+  // => currentPathSum - targetSum = oldPathSum
+  // Hence, we can search such an 'oldPathSum' in the map from the key 'currentPathSum - targetSum'.
+  if (map.has(currentPathSum - targetSum))
+    pathCount += map.get(currentPathSum - targetSum);
+
+  // This is the key step in the algorithm. We are storing the number of times the prefix sum
+  // `currentPathSum` has occurred so far.
+  map.set(currentPathSum, map.get(currentPathSum) + 1 || 1);
+
+  // Counting the number of paths from the left and right subtrees.
+  pathCount += count_paths_prefix_sum(node.left,targetSum, map, currentPathSum);
+  pathCount += count_paths_prefix_sum(node.right, targetSum, map, currentPathSum);
+
+  // Removing the current path sum from the map for backtracking.
+  // 'currentPathSum' is the prefix sum up to the current node. When we go back (i.e., backtrack), then
+  // the current node is no more a part of the path, hence, we should remove its prefix sum from the map.
+  map.set(currentPathSum, map.get(currentPathSum) - 1);
+
+  return pathCount;
 };
