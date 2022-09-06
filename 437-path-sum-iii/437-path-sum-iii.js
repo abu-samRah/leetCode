b27468c5abc1a98d084c809cce3ef7a6c44155eb
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-06 13:39:01 +0300

    Time: 203 ms (26.22%), Space: 81.3 MB (7.31%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.js
+++ 437-path-sum-iii/437-path-sum-iii.js
@@ -12,33 +12,26 @@
  * @return {number}
  */
 var pathSum = function(root, targetSum) {
-    return count_paths_recursive(root, targetSum, []);
-};
-
-function count_paths_recursive(currentNode, S, currentPath) {
-  if (currentNode === null) {
-    return 0;
-  }
-
-  // add the current node to the path
-  currentPath.push(currentNode.val);
-  let pathCount = 0,
-    pathSum = 0;
-  // find the sums of all sub-paths in the current path list
-  for (i = currentPath.length - 1; i >= 0; i--) {
-    pathSum += currentPath[i];
-    // if the sum of any sub-path is equal to 'S' we increment our path count.
-    if (pathSum === S) {
-      pathCount += 1;
+    let ans = 0
+    const dfs = (node, sum, path) =>{
+       
+        if(!node) return 0
+        path.push(node.val)
+        let pathSum = 0;
+        
+         for (let i = path.length - 1; i >= 0; i--) {
+            pathSum += path[i];
+            if (pathSum === sum) {
+              ans += 1;
+            }
+         }
+       
+        dfs(node.left  ,sum  ,[...path]) 
+        dfs(node.right ,sum ,[...path])
+        path.pop()
+       
     }
-  }
-  // traverse the left sub-tree
-  pathCount += count_paths_recursive(currentNode.left, S, currentPath);
-  // traverse the right sub-tree
-  pathCount += count_paths_recursive(currentNode.right, S, currentPath);
-
-  // remove the current node from the path to backtrack
-  // we need to remove the current node while we are going up the recursive call stack
-  currentPath.pop();
-  return pathCount;
-}
+    
+    dfs(root,targetSum,[])
+    return ans
+};
