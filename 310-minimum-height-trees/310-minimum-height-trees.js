7c8638487cc9499db7642255484823403190a4fb
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-07 09:38:36 +0300

    Time: 280 ms (59.13%), Space: 60.6 MB (98.80%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.js
+++ 310-minimum-height-trees/310-minimum-height-trees.js
@@ -3,51 +3,48 @@
  * @param {number[][]} edges
  * @return {number[]}
  */
-var findMinHeightTrees = function(n, edges) {
-  if (edges.length === 0) {
+var findMinHeightTrees = function(nodes, edges) {
+    if (nodes <= 0) {
+    return [];
+  }
+
+  if (nodes === 1) {
     return [0];
   }
-  
-  const graph = makeAdjacencyList(n, edges);
-  let m = n;
-  let leaves = [];
-  
-  for (const [node, adj] of graph) {
-    if (adj.size === 1) {
-      leaves.push(node);
-    }
-  }
-    console.log(leaves)
-  
-  while (m > 2) {
-    m -= leaves.length;
-    // The goal is to remove leaves from graph and find new leaves
-    const newLeaves = [];
     
-    for (const leaf of leaves) {
-      const neighbor = graph.get(leaf).values().next().value;
-      // a leaf only connects to one neighbor
-      graph.get(neighbor).delete(leaf);
-      // keep track of new leaves when if a node becomes a leaf after deletion
-      if (graph.get(neighbor).size === 1) {
-        newLeaves.push(neighbor);
-      }
-      graph.delete(leaf);
-    }
-    leaves = newLeaves;
-  }
-  return leaves;
-};
+  const inDegree = Array(nodes).fill(0); 
+  const graph = Array(nodes).fill(0).map(() => Array()); 
 
-function makeAdjacencyList(n, edges) {
-  const adjacencyList = new Map();
-  
-  for (let i = 0; i < n; i++) {
-    adjacencyList.set(i, new Set());
+  edges.forEach((edge) => {
+    let n1 = edge[0],
+      n2 = edge[1];
+    graph[n1].push(n2);
+    graph[n2].push(n1);
+    inDegree[n1] += 1;
+    inDegree[n2] += 1;
+  });
+
+  const leaves = [];
+  for (let i = 0; i < inDegree.length; i++) {
+    if (inDegree[i] === 1) {
+      leaves.push(i);
+    }
   }
-  for (const [v1, v2] of edges) {
-    adjacencyList.get(v1).add(v2);
-    adjacencyList.get(v2).add(v1);
+
+  let totalNodes = nodes;
+  while (totalNodes > 2) {
+    let leavesSize = leaves.length;
+    totalNodes -= leavesSize;
+    for (i = 0; i < leavesSize; i++) {
+      const front = leaves.shift();
+      graph[front].forEach((child) => {
+        inDegree[child] -= 1;
+        if (inDegree[child] === 1) {
+          leaves.push(child);
+        }
+      });
+    }
   }
-  return adjacencyList;
+
+  return leaves;
 }
