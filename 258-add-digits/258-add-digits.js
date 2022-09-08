ef6b731aad6c0de320b17bd335bb561d257be61e
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-08 14:31:28 +0300

    Time: 80 ms (88.43%), Space: 42.3 MB (98.74%) - LeetHub

--- 258-add-digits/258-add-digits.js
+++ 258-add-digits/258-add-digits.js
@@ -3,14 +3,7 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    while(num>9){
-        let sum = 0
-        for(let i=num; i>0;i= Math.floor(i/10)){
-            const curr = i % 10
-            sum += curr
-        }
-        num = sum
-    }
-    
-    return num
+    if(num == 0) return 0
+    if(num % 9 == 0) return 9
+    return num % 9
 };
