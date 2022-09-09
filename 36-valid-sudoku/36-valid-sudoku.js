19e99d948b2f60597c8d6e471436a0fec9b8167c
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-09 15:42:28 +0300

    Time: 130 ms (37.67%), Space: 45 MB (72.15%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    let valid = true
+    for(let i=0; i<9; i++){
+        let row = {}
+        let col = {}
+        let box = {}
+        for(let j =0; j<9; j++){
+            const currRow = board[i][j]
+            const currCol = board[j][i]
+            let currBox = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
+            if(row[currRow] || col[currCol] || box[currBox]){
+                valid = false
+                return false
+            }else{
+                if(currRow !== '.') row[currRow] = currRow
+                if(currCol !== '.') col[currCol] = currCol
+                if(currBox !== '.') box[currBox] = currBox
+            }
+        }
+     
+    }
+    
+   
+ 
+   
+    return valid
+};
