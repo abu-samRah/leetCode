2c932e49ad31b3ca78c13a096644884141f3f250
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-10 17:41:00 +0300

    Time: 234 ms (5.00%), Space: 53.9 MB (5.00%) - LeetHub

---
+++ 139-word-break/139-word-break.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    const seen = {}
+    const dfs = (currString) =>{
+        if(currString.length > s.length) return false
+
+        if(currString === s) return true
+        
+        for(let i =0; i<wordDict.length; i++){
+            const newString = currString + wordDict[i]
+            if(!seen[newString] && s.includes(newString)){
+                seen[newString] = true
+                if(dfs(newString)) return true
+            }
+        }
+        
+        return false
+    }
+    
+    return dfs('')
+    //console.log(seen)
+    //return x
+};
