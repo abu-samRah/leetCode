95411a16d353e5bc324d65b3bc486ac2b6307b19
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-14 09:13:01 +0300

    Time: 110 ms (53.94%), Space: 43.8 MB (99.91%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -3,21 +3,26 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-  const numsLength = nums.length
-  const permutations = []  
-  permutations.push([]);
-    
-  for (let i = 0; i < numsLength; i++) {
-    const currentNumber = nums[i];
-    const n = permutations.length;
-    for (let p = 0; p < n; p++) {
-      const oldPermutation = permutations.shift();
-      for (let j = 0; j < oldPermutation.length + 1; j++) {
-        const newPermutation = [...oldPermutation]; 
-        newPermutation.splice(j, 0, currentNumber); 
-        permutations.push(newPermutation);
+  const prems = []
+  const queue = [[]]
+
+  for(let i=0; i<nums.length; i++){
+    const currLen = queue.length
+    for(let x=0; x<currLen;x++){
+      const front = queue.shift()
+
+      for(let j=0; j<front.length +1; j++){
+        const newPrem = [...front]
+        newPrem.splice(j,0,nums[i])
+        if(newPrem.length === nums.length) {
+          prems.push(newPrem)
+        }else{
+          queue.push(newPrem)
+        }
       }
+      
     }
   }
-  return permutations;
+
+  return prems;
 };
