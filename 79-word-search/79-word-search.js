fc2977ba981b6d592b5cdcb44c35f60675aaf845
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-09-16 17:15:21 +0300

    Time: 612 ms (67.88%), Space: 42.8 MB (73.47%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -5,7 +5,7 @@
  */
 var exist = function(board, word,visited) {
     const dfs = (i,j,index,visited) =>{
-        //console.log({i,j,index})
+
         if(index === word.length) return true
         
         if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j] !== word[index] || visited[i][j])
@@ -17,7 +17,10 @@ var exist = function(board, word,visited) {
         const right = dfs(i+1,j,index+1,visited)
         const top = dfs(i,j-1,index+1,visited)
         const bottom = dfs(i,j+1,index+1,visited)
-        if(!left && !right && !top && !right) visited[i][j] = false
+        
+        if(!left && !right && !top && !right) 
+            visited[i][j] = false
+        
         return left || top || bottom || right
     }
     
