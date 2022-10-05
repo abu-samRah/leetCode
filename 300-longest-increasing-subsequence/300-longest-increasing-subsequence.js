773057837f9378f807308a55ac54be415c570bbd
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-10-05 10:08:40 +0300

    Time: 178 ms (72.18%), Space: 43.7 MB (65.20%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -6,7 +6,7 @@ var lengthOfLIS = function(nums) {
     const len = nums.length
     const LIS = new Array(len).fill(1)
     
-    for(let i=len-1; i>=0; i--){
+    for(let i=len-2; i>=0; i--){
         let j = i+1
         while(j<len){
           if(nums[i] < nums[j])
