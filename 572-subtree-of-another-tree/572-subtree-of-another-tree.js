a8c6622ec9cfa2cd920411ba008397db95579e7d
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-10-24 07:11:06 +0300

    Time: 145 ms (50.05%), Space: 49.2 MB (67.07%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.js
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -11,14 +11,13 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
+var isSubtree = function(root, subRoot) {
+    if(!root || !subRoot) return false
+    return rootChecker(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+
 const rootChecker = (first, second) => {
         if(!first && !second) return true
         if(!first || !second || first.val !== second.val) return false
         return rootChecker(first.left,second.left) && rootChecker(first.right,second.right)
     }
-
-var isSubtree = function(root, subRoot) {
-    if(!root) return false
-    return rootChecker(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-};
-
