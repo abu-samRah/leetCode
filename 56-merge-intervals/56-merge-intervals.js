16563aa10889e36b7e325629f6ce2bc6c720d181
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-11-20 19:59:37 +0200

    Time: 219 ms (16.00%), Space: 51.8 MB (14.12%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -5,16 +5,15 @@
 var merge = function(intervals) {
     if(!intervals.length) return [];
     intervals.sort((a, b) => a[0] - b[0]);
-    const result = [intervals[0]]
-    for(let i=1; i<intervals.length;i++){
-      const [prevStart, prevEnd] = result[result.length - 1]
-      const [currStart, currEnd] = intervals[i]
-      if(currStart>=prevStart && currStart<=prevEnd){
-        result.pop()
-        result.push([Math.min(prevStart,currStart),Math.max(prevEnd,currEnd)])
-      }else{
-        result.push([currStart, currEnd])
-      }
+    
+    const result = [intervals[0]];
+    
+    
+    for(let [start, end] of intervals) {
+        if(start <= result[result.length - 1][1]) {
+            const [startPrev, endPrev] = result.pop();
+            result.push([startPrev, Math.max(end, endPrev)]);
+        } else result.push([start, end]);
     }
     return result;
 };
