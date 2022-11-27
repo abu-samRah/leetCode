8909183bdbbc8664f4869cbb820d7d6eb19cb36b
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-11-27 08:04:32 +0200

    Time: 60 ms (94.54%), Space: 41.8 MB (66.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -6,9 +6,8 @@ var climbStairs = function(n) {
     let x = 1
     let y = 2
     
-    for(let i = 2; i<n; i++){
+    for(let i = 2; i<n; i++)
         [y,x] = [y+x,y]
         
-    }
     return n==1 ? 1 : y
 };
