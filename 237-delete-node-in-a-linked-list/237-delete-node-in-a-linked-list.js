91f75b2f97a1243dc53c73237aba4278d6e5e9ce
Abdullah Abusamrah <40943449+abu-samRah@users.noreply.github.com>
2022-11-29 08:39:29 +0200

    Time: 113 ms (50.21%), Space: 44.2 MB (58.02%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.js
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.js
@@ -10,11 +10,8 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function(node) {
-    // Since we know input node is not last node, so nextNode will not be null
     const nextNode = node.next;
-    // Step 2
     node.val = nextNode.val;
-    // Step 3
     node.next = nextNode.next;
     nextNode.next = null;
 };
